cb07fefe08ec93a75dfbe0d8d1beecb77aad7bd8
markmumba <markmumba01@gmail.com>
2024-01-08 17:59:11 +0300

    reversed back to the easier way of connecting to the database

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -4,8 +4,12 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/markmumba/chasebank/config"
 )
 
 func Home(c echo.Context) error {
 	return c.String(http.StatusOK, " We are running the server ")
 }
+
+
+func ( *config.DbInstance) CreateUser ()
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
+	_ "github.com/lib/pq"
 	"github.com/labstack/echo/v4"
 	"github.com/markmumba/chasebank/config"
+	"github.com/markmumba/chasebank/internal/database"
 )
-
+type DbInstance struct {
+	DB *database.Queries
+}
 
 func main() {
 	e := echo.New()
 
-	DbInstance := config.ConnectDB()
+	conn,err := sql.Open("postgres",config.Config("DATABASE_URL"))
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	DbInstance := DbInstance{
+		DB: database.New(conn),
+	}
+	defer conn.Close() 
+
 	fmt.Print(DbInstance)
 	SetupRouter(e)
 	e.Logger.Fatal(e.Start(":" + config.Config("PORT")))
