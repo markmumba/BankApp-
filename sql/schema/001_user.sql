cb89208dfd5b8b5caac458bbe5815d0a2517ae50
markmumba <markmumba01@gmail.com>
2024-01-08 12:52:50 +0300

    done with sql

--- sql/schema/001_user.sql
+++ sql/schema/001_user.sql
@@ -1,8 +1,9 @@
 -- +goose Up
 CREATE TABLE
   users (
-    user_id UUID PRIMARY KEY username VARCHAR(50) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    user_id UUID PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash CHAR(60) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     full_name VARCHAR(100) NOT NULL,
     date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
